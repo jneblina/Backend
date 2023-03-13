f97b9a67c1e63e2192231cdbd244e0fcd300926f
jneblina <janneblina7@gmail.com>
2023-03-13 14:43:13 -0700

    Agregue el proyecto

---
+++ README.md
@@ -0,0 +1,20 @@
+# MENU
+
+## Instrucciones
+
+Aqui van las instrucciones para ejecutar este proyecto
+
+Tener instalado python 3.10.0.
+
+A continuacion instalar los archivos en requirments.txt
+
+> pip install -r requirements.txt
+> pip install mysql-connector
+
+De preferencia esto debe estar en un ENV
+
+> python -m venv venv
+
+El proyecto esta realizado en flask se debe ejecutar con el siguiente comando:
+
+> flask --app 'nombre archivo' run
--- hello.py
+++ hello.py
@@ -1,16 +1,54 @@
 # pip install flask
-# flask -app 'nombre archivo' run
+# flask --app 'nombre archivo' run
 # venv\Scripts\activate.bat
-
+import json
 from flask import Flask
+from flask import jsonify
+
+
+import mysql.connector
 app = Flask(__name__)
+
+
+# Activar el virtual env
+# \venv\Scripts\activate.bat
+
+@app.route('/ligamx/equipos')
+def get_equipos():
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="14cacahuate@",
+        database="liga"
+    )
+
+    mycursor = mydb.cursor()
+
+    mycursor.execute("SELECT * FROM liga.equipos")
+
+    results = mycursor.fetchall()
+
+    mydb.close()
+
+    return jsonify(results)
 
 
 @app.route("/")
 def main():
-    return "Hola ITE"
+    return "HOLA PAMBOLEROS"
 
 
 @app.route("/bye")
 def adios():
-    return "BYE BYE"
+    return jsonify(despedida="bye bye")
+
+
+@app.route("/about")
+def about():
+    with open("liga.json", "r") as archivo:
+        contenido_json = json.load(archivo)
+        return json.dumps(contenido_json)
+
+
+if __name__ == '__main__':
+    app.run()
