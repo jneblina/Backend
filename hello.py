8e8842780e5e932c5a144f468cec177bd52542f7
jneblina <janneblina7@gmail.com>
2023-03-13 14:49:31 -0700

    Detalles estéticos

--- hello.py
+++ hello.py
@@ -5,13 +5,8 @@ import json
 from flask import Flask
 from flask import jsonify
 
-
 import mysql.connector
 app = Flask(__name__)
-
-
-# Activar el virtual env
-# \venv\Scripts\activate.bat
 
 @app.route('/ligamx/equipos')
 def get_equipos():
@@ -23,32 +18,24 @@ def get_equipos():
     )
 
     mycursor = mydb.cursor()
-
     mycursor.execute("SELECT * FROM liga.equipos")
-
     results = mycursor.fetchall()
-
     mydb.close()
-
     return jsonify(results)
-
 
 @app.route("/")
 def main():
     return "HOLA PAMBOLEROS"
 
-
 @app.route("/bye")
 def adios():
     return jsonify(despedida="bye bye")
-
 
 @app.route("/about")
 def about():
     with open("liga.json", "r") as archivo:
         contenido_json = json.load(archivo)
         return json.dumps(contenido_json)
-
 
 if __name__ == '__main__':
     app.run()
